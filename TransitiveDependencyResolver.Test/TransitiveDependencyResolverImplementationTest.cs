ce9576679d030dfd74d0dcba0724d0dc305307f8
Oren Juster <oren.juster@tipalti.com>
2021-03-30 13:51:17 +0300

    Add tests for checking

--- TransitiveDependencyResolver.Test/TransitiveDependencyResolverImplementationTest.cs
+++ TransitiveDependencyResolver.Test/TransitiveDependencyResolverImplementationTest.cs
@@ -1,4 +1,8 @@
+using DepencyTreeFromJson;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using TransitiveDependencyResolver.model;
 
 namespace TransitiveDependencyResolver.Test
 {
@@ -6,8 +10,25 @@ namespace TransitiveDependencyResolver.Test
     public class TransitiveDependencyResolverImplementationTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FullTest()
         {
+            var transitiveDependencyResolver = new TransitiveDependencyResolverImplementation();
+            var stringDependencyTree = new JsonToDependencyTreeConverter().Convert(File.ReadAllText(@"examples\projectNames.json"), x => x);
+            var result = transitiveDependencyResolver.Resolve(stringDependencyTree);
+            
+            var expected = new JsonToDependencyTreeConverter().Convert(File.ReadAllText(@"examples\finalResult.json"), x => x);
+            
+            AssertExpectedOutput(result, expected);
+        }
+
+        private void AssertExpectedOutput(IDependencyTree<string> result, IDependencyTree<string> expected)
+        {
+            CollectionAssert.AreEquivalent(expected.GetAllModules().ToArray(), result.GetAllModules().ToArray());
+            
+            foreach (var resultModule in result.GetAllModules())
+            {
+                CollectionAssert.AreEquivalent(expected.GetDependenciesOf(resultModule).ToArray(), result.GetDependenciesOf(resultModule).ToArray());
+            }
         }
     }
 }
